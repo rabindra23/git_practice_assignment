b4678421d49f38eec6399573912d194b4d0f07e3
ravi230800 <rabindra2308@gmail.com>
2022-12-16 15:31:08 +0530

    Problem 1 solved

---
+++ primeNumbers.java
@@ -0,0 +1,24 @@
+//Rabindra Kumar
+import java.util.*;
+
+class PrimeNumbers {
+
+  static boolean isPrime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i <= n / 2; i++) {
+      if (n % i == 0) return false;
+    }
+    return true;
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n=sc.nextInt();
+    if (isPrime(n)) {
+      System.out.println("The number is prime");
+    } 
+    else {
+      System.out.println("The number is not prime");
+    }
+  }
+}
